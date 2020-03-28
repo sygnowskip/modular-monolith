2f6f70fea370d0e95573dd61783a574edb9f7ed6
Paweł Sygnowski <sygnowski.p@gmail.com>
2020-03-28 11:26:24 +0100

    feature: abstractions for time provider

---
+++ Example/ModularMonolith.Time/ISystemTimeProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ModularMonolith.Time
+{
+    public interface ISystemTimeProvider
+    {
+        DateTime UtcNow { get; }
+    }
+
+    public class SystemTimeProvider : ISystemTimeProvider
+    {
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+}
