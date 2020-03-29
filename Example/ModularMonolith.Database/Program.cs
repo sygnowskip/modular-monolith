55290600d452eb3655e74a6162cff02435dc8d43
Paweł Sygnowski <sygnowski.p@gmail.com>
2020-03-29 12:06:26 +0200

    feature: database setup project

---
+++ Example/ModularMonolith.Database/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using DbUp;
+using Microsoft.Extensions.Configuration;
+using ModularMonolith.Configuration;
+
+namespace ModularMonolith.Database
+{
+    public class Program
+    {
+        public static int Main()
+        {
+            var configuration = ApplicationSettingsConfigurationProvider.Get();
+            var connectionString = configuration.GetConnectionString("Database");
+
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader = DeployChanges.To
+                .SqlDatabase(connectionString)
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                .LogToConsole()
+                .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
