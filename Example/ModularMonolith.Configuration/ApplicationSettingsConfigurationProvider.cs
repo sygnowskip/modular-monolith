a898ea8db75b752486859d6830a0e130469883f8
Paweł Sygnowski <sygnowski.p@gmail.com>
2020-03-29 12:05:13 +0200

    feature: common package for reading appsettings

---
+++ Example/ModularMonolith.Configuration/ApplicationSettingsConfigurationProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace ModularMonolith.Configuration
+{
+    public static class ApplicationSettingsConfigurationProvider
+    {
+        public static IConfigurationRoot Get(string filename = "appsettings.json")
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(filename, optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+    }
+}
