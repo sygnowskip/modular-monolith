7856811460fdd55a7c669aba6b59d3d95ada01cb
Paweł Sygnowski <sygnowski.p@gmail.com>
2020-04-18 21:37:03 +0200

    feature: service registrations around events

---
+++ Source/Hexure.EntityFrameworkCore.SqlServer/Events/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Hexure.EntityFrameworkCore.Events;
+using Hexure.EntityFrameworkCore.Events.Collecting;
+using Hexure.EntityFrameworkCore.Events.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hexure.EntityFrameworkCore.SqlServer.Events
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection WithPersistence<TDbContext>(this IServiceCollection services)
+            where TDbContext : class, ISerializedEventDbContext
+        {
+            services.AddTransient<ISerializedEventDbContext, TDbContext>();
+            services.AddTransient<ISerializedEventRepository, SerializedEventRepository>();
+            services.AddTransient<IEventCollector, EventCollector>();
+            return services;
+        }
+    }
+}
---
+++ Source/Hexure/Events/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Hexure.Events.Namespace;
+using Hexure.Events.Serialization;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hexure.Events
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDomainEvents(this IServiceCollection services)
+        {
+            services.AddTransient<IEventSerializer, EventSerializer>();
+            services.AddTransient<IEventNamespaceReader, EventNamespaceReader>();
+            return services;
+        }
+    }
+}
