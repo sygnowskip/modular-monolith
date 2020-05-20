3fb938920666f32a08e107cc646801c8dfcd98b7
Paweł Sygnowski <sygnowski.p@gmail.com>
2020-05-20 19:11:06 +0200

    feature: creating scope of DI container at the beginning of every loop of publishing domain events to RMQ

--- Source/Hexure.EventsPublisher/EventsPublisher.cs
+++ Source/Hexure.EventsPublisher/EventsPublisher.cs
@@ -15,23 +15,20 @@ namespace Hexure.EventsPublisher
     {
         private readonly int _defaultBatchSize;
         private readonly TimeSpan _defaultDelay;
-        private readonly IBus _bus;
         private readonly IBusControl _busControl;
-        private readonly ISerializedEventRepository _serializedEventRepository;
         private readonly ISystemTimeProvider _systemTimeProvider;
-        private readonly ITransactionProvider _transactionProvider;
         private readonly IEventDeserializer _eventDeserializer;
+        private readonly IServiceProvider _serviceProvider;
 
         public EventsPublisher(int defaultBatchSize, TimeSpan defaultDelay, IServiceProvider serviceProvider)
         {
-            _serializedEventRepository = serviceProvider.GetRequiredService<ISerializedEventRepository>();
-            _busControl = serviceProvider.GetRequiredService<IBusControl>();
-            _bus = serviceProvider.GetRequiredService<IBus>();
-            _systemTimeProvider = serviceProvider.GetRequiredService<ISystemTimeProvider>();
-            _transactionProvider = serviceProvider.GetRequiredService<ITransactionProvider>();
-            _eventDeserializer = serviceProvider.GetRequiredService<IEventDeserializer>();
             _defaultBatchSize = defaultBatchSize;
             _defaultDelay = defaultDelay;
+
+            _serviceProvider = serviceProvider;
+            _busControl = serviceProvider.GetRequiredService<IBusControl>();
+            _systemTimeProvider = serviceProvider.GetRequiredService<ISystemTimeProvider>();
+            _eventDeserializer = serviceProvider.GetRequiredService<IEventDeserializer>();
         }
 
         public async Task RunAsync()
@@ -42,32 +39,42 @@ namespace Hexure.EventsPublisher
             {
                 while (true)
                 {
-                    await _transactionProvider.BeginTransactionAsync();
-                    Console.WriteLine($"{DateTime.UtcNow} Publishing (batch size: {_defaultBatchSize})...");
-                    var eventsToPublish = await _serializedEventRepository.GetUnpublishedEventsAsync(_defaultBatchSize);
-
-                    foreach (var serializedEventEntity in eventsToPublish)
+                    using (var scopedContainer = _serviceProvider.CreateScope())
                     {
-                        try
-                        {
-                            await _eventDeserializer.Deserialize(serializedEventEntity.SerializedEvent)
-                                .OnSuccess(async @event =>
-                                {
-                                    await _bus.Publish(@event);
-                                });
+                        var transactionProvider =
+                            scopedContainer.ServiceProvider.GetRequiredService<ITransactionProvider>();
+                        var serializedEventRepository = scopedContainer.ServiceProvider
+                            .GetRequiredService<ISerializedEventRepository>();
 
-                            await serializedEventEntity.MarkAsProcessed(_systemTimeProvider.UtcNow)
-                                .OnSuccess(() => _serializedEventRepository.SaveChangesAsync())
-                                .OnFailure(errors =>
-                                    throw new InvalidOperationException(JsonConvert.SerializeObject(errors)));
-                        }
-                        catch (Exception ex)
+                        await transactionProvider.BeginTransactionAsync();
+                        Console.WriteLine($"{DateTime.UtcNow} Publishing (batch size: {_defaultBatchSize})...");
+                        var eventsToPublish = await serializedEventRepository.GetUnpublishedEventsAsync(_defaultBatchSize);
+
+                        foreach (var serializedEventEntity in eventsToPublish)
                         {
-                            Console.WriteLine(ex.ToString());
+                            try
+                            {
+                                await _eventDeserializer.Deserialize(serializedEventEntity.SerializedEvent)
+                                    .OnSuccess(async @event =>
+                                    {
+                                        await _busControl.Publish(@event);
+                                    });
+
+                                await serializedEventEntity.MarkAsProcessed(_systemTimeProvider.UtcNow)
+                                    .OnSuccess(() => serializedEventRepository.SaveChangesAsync())
+                                    .OnFailure(errors =>
+                                        throw new InvalidOperationException(JsonConvert.SerializeObject(errors)));
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.ToString());
+                            }
                         }
+
+                        await transactionProvider.CommitTransactionAsync();
+
                     }
 
-                    await _transactionProvider.CommitTransactionAsync();
                     await Task.Delay(_defaultDelay);
                 }
             }
