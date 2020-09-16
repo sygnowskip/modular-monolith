3f2c8e64121461ddf5fef9f845e6909b8f597a32
Paweł Sygnowski <sygnowski.p@gmail.com>
2020-09-16 19:22:51 +0200

    feature: exams language and events

---
+++ Example/ModularMonolith.Exams.Language/ExamId.cs
@@ -0,0 +1,11 @@
+﻿using Hexure.Identifiers.Numeric;
+
+namespace ModularMonolith.Exams.Language
+{
+    public class ExamId : Identifier
+    {
+        protected ExamId(long value) : base(value)
+        {
+        }
+    }
+}
