67c6c9cf32dcce92c56f7ce4466de49dd7366ab5
Paweł Sygnowski <sygnowski.p@gmail.com>
2020-11-01 11:48:36 +0100

    feature: logger with output to console for testing purposes
    a

---
+++ Source/Hexure.Testing/Logging/NUnitLoggerProvider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Concurrent;
+using Microsoft.Extensions.Logging;
+
+namespace Hexure.Testing.Logging
+{
+    public class NUnitLoggerProvider : ILoggerProvider
+    {
+        private readonly ConcurrentDictionary<string, NUnitLogger> _loggers = new ConcurrentDictionary<string, NUnitLogger>();
+
+        public void Dispose()
+        {
+            _loggers.Clear();
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return _loggers.GetOrAdd(categoryName, name => new NUnitLogger(name));
+        }
+    }
+}
