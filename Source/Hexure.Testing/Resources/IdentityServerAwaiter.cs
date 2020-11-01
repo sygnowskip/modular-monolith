8d86e3108bf746874fa6f9ccf58755df141ecdec
Paweł Sygnowski <sygnowski.p@gmail.com>
2020-11-01 10:25:31 +0100

    feature: resource awaiters

---
+++ Source/Hexure.Testing/Resources/IdentityServerAwaiter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Hexure.Testing.Resources
+{
+    public class IdentityServerAwaiterDiscoveryDocument
+    {
+        [JsonProperty("scopes_supported")] public ICollection<string> ScopesSupported { get; set; }
+    }
+
+    public class IdentityServerAwaiter
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILogger<IdentityServerAwaiter> _logger;
+
+        public IdentityServerAwaiter(ILogger<IdentityServerAwaiter> logger, HttpClient httpClient)
+        {
+            _logger = logger;
+            _httpClient = httpClient;
+        }
+
+        public async Task WaitForScopeAsync(string authorityUri, string scope)
+        {
+            var discoveryDocumentUri = new Uri(new Uri(authorityUri), "/.well-known/openid-configuration");
+            var acceptingConnectionsAndScopeExist = false;
+            while (!acceptingConnectionsAndScopeExist)
+            {
+                try
+                {
+                    var result = await _httpClient.GetAsync(discoveryDocumentUri);
+                    if (result.IsSuccessStatusCode)
+                    {
+                        _logger.LogInformation($"Connection accepted! ({discoveryDocumentUri.AbsoluteUri})");
+                        if (await ScopeExist(result, scope))
+                        {
+                            _logger.LogInformation($"Scope exist!");
+                            acceptingConnectionsAndScopeExist = true;
+                        }
+                        else
+                        {
+                            _logger.LogInformation($"Scope does not exist, retrying...");
+                        }
+                    }
+                }
+                catch
+                {
+                    acceptingConnectionsAndScopeExist = false;
+                    _logger.LogInformation($"Connection refused, retrying... ({discoveryDocumentUri.AbsoluteUri})");
+                }
+
+                await Task.Delay(TimeSpan.FromMilliseconds(2500));
+            }
+        }
+
+        private async Task<bool> ScopeExist(HttpResponseMessage result, string scope)
+        {
+            var response = await result.Content.ReadAsStringAsync();
+            var discoveryDocument = JsonConvert.DeserializeObject<IdentityServerAwaiterDiscoveryDocument>(response);
+            return discoveryDocument.ScopesSupported != null && discoveryDocument.ScopesSupported.Contains(scope);
+        }
+    }
+}
---
+++ Source/Hexure.Testing/Resources/RabbitMqResourceAwaiter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Hexure.Testing.Resources
+{
+    public class RabbitMqResourceConfiguration
+    {
+        public string RabbitMqManagementUrl { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class RabbitMqResourceAwaiter
+    {
+        private readonly HttpClient _httpClient;
+        private readonly RabbitMqResourceConfiguration _configuration;
+        private readonly ILogger<RabbitMqResourceAwaiter> _logger;
+
+        public RabbitMqResourceAwaiter(HttpClient httpClient, IOptions<RabbitMqResourceConfiguration> configuration, ILogger<RabbitMqResourceAwaiter> logger)
+        {
+            _httpClient = httpClient;
+            _logger = logger;
+            _configuration = configuration.Value;
+            ConfigureHttpClientAuthentication();
+        }
+
+        public async Task WaitForConnectionAsync()
+        {
+            var alivenessUri = new Uri(new Uri(_configuration.RabbitMqManagementUrl), "api/aliveness-test/%2F");
+            var acceptingConnections = false;
+            while (!acceptingConnections)
+            {
+                try
+                {
+                    var result = await _httpClient.GetAsync(alivenessUri);
+                    if (result.IsSuccessStatusCode)
+                    {
+                        acceptingConnections = true;
+                        _logger.LogInformation($"Connection accepted! ({_configuration.RabbitMqManagementUrl})");
+                    }
+                }
+                catch
+                {
+                    acceptingConnections = false;
+                    _logger.LogInformation($"Connection refused, retrying... ({_configuration.RabbitMqManagementUrl})");
+                }
+
+                await Task.Delay(TimeSpan.FromMilliseconds(2500));
+            }
+        }
+
+        private void ConfigureHttpClientAuthentication()
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                Convert.ToBase64String(
+                    System.Text.Encoding.ASCII.GetBytes($"{_configuration.Username}:{_configuration.Password}")));
+        }
+    }
+}
