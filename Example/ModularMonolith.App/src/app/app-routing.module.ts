1233035247908ab5a935f8fa6bd5bd93e2524e2a
Aneta <aneta.dabrowska@puredev.eu>
2020-12-28 17:59:27 +0100

    Preliminary html template & initial routing

--- Example/ModularMonolith.App/src/app/app-routing.module.ts
+++ Example/ModularMonolith.App/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'planexam',
+    loadChildren: () => import('./views/plan-exam/plan-exam.module').then((m) => m.PlanExamModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ Example/ModularMonolith.App/src/app/views/plan-exam/plan-exam-routing.module.ts
@@ -0,0 +1,8 @@
+import {Routes} from '@angular/router';
+import {PlanExamComponent} from './plan-exam.component';
+
+export const routes: Routes = [
+  {path: '', component: PlanExamComponent}
+];
+
+
