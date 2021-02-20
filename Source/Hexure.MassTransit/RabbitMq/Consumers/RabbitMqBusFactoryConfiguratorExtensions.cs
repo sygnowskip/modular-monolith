3474a5b8270fddcf7390ce5bb6e916edb309281a
Paweł Sygnowski <sygnowski.p@gmail.com>
2021-02-20 12:13:34 +0100

    chore: transaction on consumers for workers

--- Source/Hexure.MassTransit/RabbitMq/Consumers/RabbitMqBusFactoryConfiguratorExtensions.cs
+++ Source/Hexure.MassTransit/RabbitMq/Consumers/RabbitMqBusFactoryConfiguratorExtensions.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 using System.Reflection;
 using MassTransit;
 using MassTransit.RabbitMqTransport;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Hexure.MassTransit.RabbitMq.Consumers
 {
     public static class RabbitMqBusFactoryConfiguratorExtensions
     {
         public static void ReceiveEndpointForEachConsumer(this IRabbitMqBusFactoryConfigurator configurator,
-            IServiceProvider provider, string queuePrefix, ICollection<Assembly> fromAssemblies,
+            IBusRegistrationContext provider, string queuePrefix, ICollection<Assembly> fromAssemblies,
             Action<IRabbitMqReceiveEndpointConfigurator> endpointConfiguration = null)
         {
             foreach (var consumer in ConsumersProvider.GetConsumers(fromAssemblies))
             {
                 configurator.ReceiveEndpoint($"{queuePrefix}:{consumer.FullName}", endpointConfigurator =>
                 {
-                    endpointConfigurator.Consumer(consumer, type => provider.CreateScope().ServiceProvider.GetRequiredService(type));
+                    endpointConfigurator.ConfigureConsumer(provider, consumer);
                     endpointConfiguration?.Invoke(endpointConfigurator);
                 });
             }
---
+++ Source/Hexure.MassTransit/RabbitMq/Transactions/TransactionFilter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using GreenPipes;
+using Hexure.EntityFrameworkCore;
+using MassTransit;
+
+namespace Hexure.MassTransit.RabbitMq.Transactions
+{
+    public class TransactionFilter<TMessage> : IFilter<ConsumeContext<TMessage>>
+        where TMessage : class
+    {
+        private readonly ITransactionProvider _transactionProvider;
+
+        public TransactionFilter(ITransactionProvider transactionProvider)
+        {
+            _transactionProvider = transactionProvider;
+        }
+
+        public async Task Send(ConsumeContext<TMessage> context, IPipe<ConsumeContext<TMessage>> next)
+        {
+            try
+            {
+                await _transactionProvider.BeginTransactionAsync();
+                await next.Send(context);
+                await _transactionProvider.CommitTransactionAsync();
+            }
+            catch (Exception ex)
+            {
+                //TODO: Logging
+                Console.WriteLine(ex.ToString());
+                await _transactionProvider.RollbackTransactionAsync();
+                throw;
+            }
+        }
+
+        public void Probe(ProbeContext context)
+        {
+            context.CreateFilterScope("transaction-filter");
+        }
+    }
+}
