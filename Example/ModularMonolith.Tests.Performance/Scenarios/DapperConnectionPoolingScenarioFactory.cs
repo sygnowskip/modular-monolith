6854433f8bfec54b13367eb683ebd6c08a8152e1
Paweł Sygnowski <sygnowski.p@gmail.com>
2021-07-17 15:43:12 +0200

    tests: dapper connection pooling performance tests
    chore: minor naming fixes

---
+++ Example/ModularMonolith.Tests.Performance/Scenarios/DapperConnectionPoolingScenarioFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using NBomber.Contracts;
+using NBomber.CSharp;
+using NBomber.Plugins.Http.CSharp;
+
+namespace ModularMonolith.Tests.Performance.Scenarios
+{
+    public static class DapperConnectionPoolingScenarioFactory
+    {
+        private static readonly TimeSpan WarmUpDuration = TimeSpan.FromSeconds(5);
+
+        private static LoadSimulation[] BuildLoadSimulations() => new[]
+        {
+            Simulation.RampPerSec(rate: 100, during: TimeSpan.FromMinutes(3))
+        };
+
+        private static IStep BuildStep()
+        {
+            return Step.Create("Single", HttpClientFactory.Create(), async context =>
+            {
+                var request = Http.CreateRequest(HttpMethod.Get.Method, "https://localhost:5002/api/exams");
+                return await Http.Send(request, context);
+            });
+        }
+
+        public static Scenario Build()
+        {
+            return ScenarioBuilder
+                .CreateScenario("Dapper connection pooling tests", BuildStep())
+                .WithWarmUpDuration(WarmUpDuration)
+                .WithLoadSimulations(BuildLoadSimulations());
+        }
+    }
+}
